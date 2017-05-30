1b4470bd56c0e8b0052173dc8507e8fe9ae065fa
roger <roger.herikstad@gmail.com>
2017-05-30 13:47:22 +0800

    Adds functions for creating states and state transitions for multiple neurons

--- src/utils.jl
+++ src/utils.jl
@@ -44,3 +44,63 @@ function logsumexpl{T<:Real}(X::Array{T,1},i=1)
         end
      end
 end
+
+function generate_states(N,K)
+    states = zeros(Int64,N,1+N*(K-1) + N*(N-1)*(K-1)*(K-1))
+    k = 2
+    for i in 1:N
+        for k1 in 1:K-1
+            states[i,k] = k1
+            k += 1
+        end
+    end
+    for i in 1:N
+       for j in 1:N
+           if i==j
+               continue
+           end
+           for k1 in 1:K-1
+               for k2 in 1:K-1
+                   states[i,k] = k1
+                   states[j,k] = k2
+                   k += 1
+               end
+           end
+       end
+   end
+   states
+end
+
+function isvalid_transition(states,K,lp,j1,j2)
+    lpt = 0.0
+    for i in 1:size(states,1)
+        s1 = states[i,j1]
+        s2 = states[i,j2]
+        lpi = lp[i]
+        if s1 == s2 == 0
+            lpt += log1p(-exp(lpi))
+        elseif s1 == 0 && s2 == 1
+            lpt += lpi
+        elseif (s2 - s1 == 1) || (s1 == K-1 && s1 == K-1)
+            lpt += 0.0
+        else #other transitions simply add 0.0
+            lpt = -Inf #impossible transition
+            break
+        end
+    end
+    lpt
+end
+
+function get_valid_transitions(states::Array{Int64,2}, K,lp)
+    idx = Array{Tuple{Int64, Int64, Float64}}(0)
+    nstates = size(states,2)
+    for i in 1:nstates
+        for j in 1:nstates
+            aa = isvalid_transition(states, K, lp,i,j)
+            if isfinite(aa)
+                push!(idx, (i,j,aa))
+            end
+        end
+    end
+    idx
+end
