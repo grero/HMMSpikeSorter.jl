2b380f60b459c96d915025c89e53b6c90f189cee
roger <roger.herikstad@gmail.com>
2017-06-05 17:41:09 +0800

    Adds functions for creating signals

--- src/utils.jl
+++ src/utils.jl
@@ -45,3 +45,41 @@ function logsumexpl{T<:Real}(X::Array{T,1},i=1)
      end
 end
 
+create_spike_template(nstates::Integer) = create_template(nstates, 1.0, 0.8, 0.2)
+function create_spike_template(nstates::Integer, a::Real, b::Real, c::Real)
+    x = linspace(0,1.5,nstates)
+    y = a*sin(2*pi*x).*exp(-(b-x).^2/c)
+    return y
+end
+
+function create_signal(N::Integer, sigma::Number, pp::Array{Float64,1}, templates::Array{Float64,2})
+    nstates,ncells = size(templates)
+    state = zeros(Int64, ncells)
+    S = rand(Normal(0,sigma),N)
+    active_cell = 0
+    for i in 1:length(S)
+        if active_cell == 0
+            for j in 1:ncells
+                r = rand()
+                if pp[j] > r
+                    state[j] = 1
+                    active_cell = j
+                    break
+                else
+                    active_cell = 0
+                    state[j] = 0
+                end
+            end
+        end
+        if active_cell > 0
+            S[i] += templates[state[active_cell], active_cell]
+            state[active_cell] += 1
+            if state[active_cell] > nstates
+                state[active_cell] = 0
+                active_cell = 0
+            end
+        end
+    end
+    return S
+end
+
