1f667a810cf0888c241fc1f91d2aa810e450148d
roger <roger.herikstad@gmail.com>
2017-06-06 19:56:42 +0800

    Fixes bug related to module name change

--- src/example.jl
+++ src/example.jl
@@ -1,11 +1,11 @@
 using PyPlot
-include("HMM.jl")
+include("HMMSpikeSorter.jl")
 
-temp1 = HMM.create_spike_template(60,3.0, 0.8, 0.2)
-temp2 = HMM.create_spike_template(60,4.0, 0.3, 0.2)
+temp1 = HMMSpikeSorter.create_spike_template(60,3.0, 0.8, 0.2)
+temp2 = HMMSpikeSorter.create_spike_template(60,4.0, 0.3, 0.2)
 temps = cat(2, temp1, temp2)
 
-S = HMM.create_signal(20_000, 0.3, [0.003, 0.001], temps)
+S = HMMSpikeSorter.create_signal(20_000, 0.3, [0.003, 0.001], temps)
 f = plt[:figure]()
 ax1 = f[:add_subplot](121)
 ax2 = f[:add_subplot](122)
@@ -19,9 +19,9 @@ function update_plot(x)
     ax2[:legend]()
 end
 
-lA, μ, σ = HMM.train_model(S, 3, 60, false, 10,update_plot)
-x,T2, T1 = HMM.viterbi(S, lA, μ, σ)
-Y2 = HMM.reconstruct_signal(x, lA, μ, σ)
+lA, μ, σ = HMMSpikeSorter.train_model(S, 3, 60, false, 10,update_plot)
+x,T2, T1 = HMMSpikeSorter.viterbi(S, lA, μ, σ)
+Y2 = HMMSpikeSorter.reconstruct_signal(x, lA, μ, σ)
 
 ax1[:plot](Y2;label="Reconstructed")
 
