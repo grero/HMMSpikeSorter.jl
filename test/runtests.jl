6295c64d4dbfb622f008b1101f7ae4fb3842b344
roger <roger.herikstad@gmail.com>
2017-06-06 10:16:10 +0800

    Fixes an issue with module naming in the test suite

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,13 @@ using Base.Test
 using HMMSpikeSorter
 srand(0)
 
-temp1 = HMM.create_spike_template(60,3.0, 0.8, 0.2)
-temp2 = HMM.create_spike_template(60,4.0, 0.3, 0.2)
+temp1 = HMMSpikeSorter.create_spike_template(60,3.0, 0.8, 0.2)
+temp2 = HMMSpikeSorter.create_spike_template(60,4.0, 0.3, 0.2)
 temps = cat(2, temp1, temp2)
-S = HMM.create_signal(20_000, 0.3, [0.003, 0.001], temps)
-lA, μ, σ = HMM.train_model(S, 3, 60, false, 5,update_plot;verbose=0)
-x,T2, T1 = HMM.viterbi(S, lA, μ, σ)
-Y2 = HMM.reconstruct_signal(x, lA, μ, σ)
+S = HMMSpikeSorter.create_signal(20_000, 0.3, [0.003, 0.001], temps)
+lA, μ, σ = HMMSpikeSorter.train_model(S, 3, 60, false, 5,update_plot;verbose=0)
+x,T2, T1 = HMMSpikeSorter.viterbi(S, lA, μ, σ)
+Y2 = HMMSpikeSorter.reconstruct_signal(x, lA, μ, σ)
 
 @test_approx_eq 1-std(Y2-S)/std(S) 0.5764879059017742
 
