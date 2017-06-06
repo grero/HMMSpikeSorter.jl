3ac94497df645a7a9cb6df4fa0080fc28bc10833
roger <roger.herikstad@gmail.com>
2017-06-06 10:18:39 +0800

    Removes callback function from the training step

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ temp1 = HMMSpikeSorter.create_spike_template(60,3.0, 0.8, 0.2)
 temp2 = HMMSpikeSorter.create_spike_template(60,4.0, 0.3, 0.2)
 temps = cat(2, temp1, temp2)
 S = HMMSpikeSorter.create_signal(20_000, 0.3, [0.003, 0.001], temps)
-lA, μ, σ = HMMSpikeSorter.train_model(S, 3, 60, false, 5,update_plot;verbose=0)
+lA, μ, σ = HMMSpikeSorter.train_model(S, 3, 60, false, 5;verbose=0)
 x,T2, T1 = HMMSpikeSorter.viterbi(S, lA, μ, σ)
 Y2 = HMMSpikeSorter.reconstruct_signal(x, lA, μ, σ)
 
