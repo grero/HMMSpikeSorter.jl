23431dbf2370acc9f519cd70f84628d37ec68bae
Roger Herikstad <roger.herikstad@gmail.com>
2017-06-23 18:39:44 +0800

    Adds functions to save model and spike trains from model

--- src/HMMSpikeSorter.jl
+++ src/HMMSpikeSorter.jl
@@ -1,6 +1,9 @@
 module HMMSpikeSorter
 using StatsBase
+using FileIO
+using MAT
 import StatsBase.fit, StatsBase.predict, StatsBase.loglikelihood, StatsBase.model_response
+import FileIO.save
 using Distributions
 include("utils.jl")
 include("types.jl")
@@ -8,4 +11,5 @@ include("baumwelch.jl")
 include("viterbi.jl")
 include("reconstruction.jl")
 include("fit.jl")
+include("extraction.jl")
 end #module
---
+++ src/extraction.jl
@@ -0,0 +1,28 @@
+function extract_spiketimes(model::HMMSpikingModel)
+    pidx = Array{Array{Int64,1}}(0)
+    for i in 1:model.template_model.state_matrix.N
+        qidx = indmin(model.template_model.μ[:,i])
+        sidx = find(any(model.template_model.state_matrix.states .== qidx,1))
+        _pidx = findin(model.ml_seq,sidx)
+        push!(pidx, _pidx)
+    end
+    pidx
+end
+
+function extract_units(model::HMMSpikingModel, channel::Integer)
+    pidx = extract_spiketimes(model::HMMSpikingModel)
+    units = Dict()
+    for i in 1:length(pidx)
+        unit_name = @sprintf "g%03dc%02d_spiketrain.mat" channel i
+        units[unit_name] = Dict("timestamps" => pidx[i],
+                                "sampling_rate" => 40000.0,
+                                "waveform" => model.template_model.μ[:,i])
+    end
+    units
+end
+
+function save_units(units::Dict)
+    for (k,v) in units
+        MAT.matwrite("sorted/$k", v)
+    end
+end
