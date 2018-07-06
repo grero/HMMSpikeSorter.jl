ab469e8a8fa1b2f789a43cc2b08e77527a963799
roger <roger.herikstad@gmail.com>
2018-07-06 19:20:18 +0800

    Updates test to use specific RNG

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,11 +14,12 @@ srand(0)
 #@test_approx_eq 1-std(Y2-S)/std(S) 0.5764879059017742
 
 function test_viterbi()
+    rng = MersenneTwister(UInt32(1234))
     temp1 = HMMSpikeSorter.create_spike_template(60,3.0, 0.8, 0.2)
     temp2 = HMMSpikeSorter.create_spike_template(60,4.0, 0.3, 0.2)
     temps = cat(2, temp1, temp2)
     pp = [0.003, 0.001]
-    S = HMMSpikeSorter.create_signal(20_000, 0.3, pp, temps)
+    S = HMMSpikeSorter.create_signal(20_000, 0.3, pp, temps;rng=rng)
     lA = HMMSpikeSorter.StateMatrix(2, 60, log.(pp), true)
     templates = HMMSpikeSorter.HMMSpikeTemplateModel(lA, temps, 0.3)
     modelf = HMMSpikeSorter.fit(HMMSpikeSorter.HMMSpikingModel, templates, S)
@@ -28,7 +29,7 @@ end
 
 @testset "Viterbi" begin
     aa = test_viterbi()
-    @test aa ≈ 0.5753193438558597
+    @test aa ≈ 0.5642293153292788 
 end
 
 @testset "Unroll" begin
